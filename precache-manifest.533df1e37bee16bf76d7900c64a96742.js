c2ae72804d9c0921af823fca98453a142829849a
QuiK000 <teamquiksteam@gmail.com>
2020-01-19 14:39:38 +0200

    first commit

---
+++ precache-manifest.533df1e37bee16bf76d7900c64a96742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7721359a4174dc1860ee691e1ca93d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a35b0fd6f5e910911542",
+    "url": "/static/css/main.e1465dd1.chunk.css"
+  },
+  {
+    "revision": "ee666aa575a5d72ceb11",
+    "url": "/static/js/2.34c9e9da.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/static/js/2.34c9e9da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a35b0fd6f5e910911542",
+    "url": "/static/js/main.0b266f0b.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
